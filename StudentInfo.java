b46f81d90341360cbd32bf29fd627b86970c51c6
Obaida <15270128+TA4life@users.noreply.github.com>
2019-07-07 22:06:24 -0400

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,20 @@
+public class StudentInfo {
+
+    /**
+    * Displays the student information: student name, id, section, etc for each
+    * member of the team.
+    */
+
+    public static void display() {
+
+        System.out.println("************************************************************");
+        System.out.println("*          Obaida Kandakji                                 *");
+        System.out.println("*          8709800                                         *");
+        System.out.println("*          ITI 1121                                        *");
+        System.out.println("*                                                          *");
+        System.out.println("************************************************************");
+        System.out.println();
+
+    }
+
+}
